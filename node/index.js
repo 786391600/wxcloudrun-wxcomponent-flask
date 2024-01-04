5ca9454c8452a9d8dd4563503307684d9681aa3a
wangtao <l>
2024-01-04 21:14:30 +0800

    feat: new

--- node/index.js
+++ node/index.js
@@ -2,12 +2,19 @@
  * @Author: Wangtao
  * @Date: 2024-01-03 18:40:08
  * @LastEditors: Wangtao
- * @LastEditTime: 2024-01-04 20:46:59
+ * @LastEditTime: 2024-01-04 21:13:41
  */
 const Koa = require('koa');
 const app = new Koa();
+const koaRoute = require('koa-route');
+
 
 console.log('node 服务==============')
+
+koaRoute.post('/action', (ctx) => {
+  ctx.response.status = 200
+  ctx.response.body = 'test';
+})
 app.use(async ctx => {
   ctx.body = 'Hello World';
 });

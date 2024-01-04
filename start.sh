22a44f446f31d870e813e0f670b0ffc0b9cd33b6
wangtao <l>
2024-01-04 16:14:33 +0800

    feat: new

--- start.sh
+++ start.sh
@@ -3,16 +3,13 @@
  # @Author: Wangtao
  # @Date: 2024-01-03 18:17:46
  # @LastEditors: Wangtao
- # @LastEditTime: 2024-01-03 20:46:00
+ # @LastEditTime: 2024-01-04 16:10:53
 ### 
 echo '进来了'
 python3 run.py 0.0.0.0 8080 &
 cd /wxcloudrun-wxcomponent
 ./main
 
-
-cd /node
-pm2 start index.js
 # start_server(){
 #   if check_port 8083
 #   then
